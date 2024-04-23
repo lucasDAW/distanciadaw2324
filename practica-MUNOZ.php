b227ce4a2acc17ec05859115871f269ce995e5c1
Lucas Antonio Munoz Albertos <lucasmual93@gmail.com>
2024-04-23 11:29:39 +0200

    primer commit

---
+++ practica-MUNOZ.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+* Este  script PHP contiene funciones de prueba
+* Script creado por Lucas Antonio Muñoz Albertos
+* @package Tarea Unidad 5
+* @version 1.0
+* @author Lucas Antonio Muñoz Albertos
+*/
+
+/**
+* Funcion que suma dos números y devuelve el resultado
+* @param float $numero1 primer numero para sumar
+* @param float $numero2 segundo numero para sumar
+* @return float devuelve la suma de la suma de los números
+*/
+function suma($numero1,$numero2){
+	return $numero1+$numero2;
+}
+
+/**
+* Funcion que muestra un saludo con el nombre introducido
+* @param string $nombre nombre introducido
+* @return string devuelve la cadena 'hola '+ [nombre] 
+*/
+function saludo($nombre){
+	return "Hola, "+$nombre;
+}
+/**
+* Funcion que obtiene el factorial de un número introducido
+* @param integer $numero numero sobre el que se calcula el factorial n! 
+* @return integer devuelve el factorial del número 
+*/
+function factorial($numero){
+$factorial =1;
+	for($i=1; $i<=$numero;$i++){
+		$factorial = $factorial*$i;
+	}
+return $factorial;
+}
+/**
+* Esta es una anotación que solo será visible en la documentación para desarrolladores 
+* No se incluirá en la documentación generada para los ususarios finales
+* @internal Esta anotación es para uso interno del desarrollo
+*/
+?>
